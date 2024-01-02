b24223939e9eb55c91b04656207081c48397b98e
Akash Chakraborty <chakraborty.akash@sonaliintellect.com>
2024-01-02 18:13:28 +0600

    myAccounts and favorites page designed completely

---
+++ Project/js/project.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.floatingMobile').hide();
+    $('input[type="radio"]').change(function(){
+        if($(this).val()==='option2'){
+            $('.floatingMobile').show();
+            $('.floatingEmail').hide();
+        }
+        else{
+            $('.floatingEmail').show();
+            $('.floatingMobile').hide();
+        }
+    });
+});
+// Password Visibility
+$('#passwordReveal').on('click',function(){
+    const type = $('#floatingPassword').attr('type') === 'password' ? 'text' : 'password';
+    $('#floatingPassword').attr('type', type);
+    if(type === 'password'){
+        $('.icon').removeClass("fa-eye");
+        $('.icon').addClass("fa-eye-slash");
+    }
+    else if (type === 'text'){
+        $('.icon').removeClass("fa-eye-slash");
+        $('.icon').addClass("fa-eye");
+    }
+});
+
+
+    
