5ef3eefd8031dd0d91aabb7210bb5d1a820d44ab
Akash Chakraborty <chakraborty.akash@sonaliintellect.com>
2024-01-21 11:52:47 +0600

    Up to date with IB_UI

--- Project/js/project.js
+++ Project/js/project.js
@@ -10,6 +10,34 @@ $(document).ready(function(){
             $('.floatingMobile').hide();
         }
     });
+    $('#main').hide();
+    // Attach a change event handler to the checkbox
+    $('#virtualKeyboard').change(function() {
+        displayKeyboard();
+      });
+
+      // Attach a click event handler to the input field
+      $('#floatingPassword').click(function() {
+        displayKeyboard();
+      });
+
+      // Function to update keyboard visibility based on checkbox and input field state
+      function displayKeyboard() {
+        // Check if the checkbox is checked and input field is clicked
+        if ($('#virtualKeyboard').prop('checked') & $('#floatingPassword').is(':focus')) {
+          // Show the div
+          $('#main').show();
+        } else {
+          // Hide the div
+          $('#main').hide();
+        }
+    };
+    // Hide the keyboard when clicking outside the password field
+    // $(document).click(function(event) {
+    //     if (!$(event.target).closest('#floatingPassword').length) {
+    //       $('#main').hide();
+    //     }
+    // });
 });
 // Password Visibility
 $('#passwordReveal').on('click',function(){
